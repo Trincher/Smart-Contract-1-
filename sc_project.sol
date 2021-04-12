14ad2d315e40762abf3f3c8d19a9109222b6a573
Trincher <74297486+Trincher@users.noreply.github.com>
2021-04-12 07:11:05 +0100

    Add files via upload

---
+++ sc_project.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.6.0;
+
+contract Product {
+
+    enum Statuses {Paid, NotPaid}
+    Statuses public currentStatus;
+
+    event Bought(address _buyer, uint _value);
+
+    address payable public owner;
+
+    constructor() public {
+        owner = msg.sender;
+        currentStatus = Statuses.NotPaid;
+    }
+
+    modifier onlyWhileNotPaid{
+        require(currentStatus == Statuses.NotPaid, "Impossible to proceed.");
+        _;
+    }
+
+    modifier costs(uint _amount) {
+        require(msg.value = _amount, "Not enough currency provided.");
+        _;
+    }
+
+    function pay() external payable onlyWhileNotPaid costs(10 ether) {
+        currentStatus = Statuses.Paid;
+        owner.transfer(msg.value);
+        emit Bought(msg.sender, msg.value);
+    }
+
+}
